230dc52f9d5a117424b5cac08e35917d3fa6bae6
UdaraJayawardena <udarajayawardena5@gmail.com>
2019-06-18 11:32:27 +0530

    Crud Operations V1

--- src/app/components/customercontent/customercontent.component.ts
+++ src/app/components/customercontent/customercontent.component.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {CustomerService} from '../../service/customer.service';
+import {Customer} from '../../dto/customer';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-customercontent',
@@ -6,13 +9,37 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./customercontent.component.css']
 })
 export class CustomercontentComponent implements OnInit {
+  customers: Customer[] = [];
+  selectedCustomer: Customer = new Customer('', '', '', 0);
 
+  @ViewChild('txtId') txtId: ElementRef;
+  @ViewChild('frmCustomer') frmCustomer: NgForm;
 
-  constructor() {
+  constructor(private customerService: CustomerService) {
   }
 
   ngOnInit() {
+    this.customerService.getAllCustomers().subscribe(customers => {
+      this.customers = customers;
+    });
+  }
 
+  saveCustomer(): void {
+    if (!this.frmCustomer.invalid) {
+
+      this.customerService.saveCustomer(this.selectedCustomer)
+        .subscribe(resp => {
+          if (resp) {
+            alert('Customer has been saved successfully');
+            this.customers.push(this.selectedCustomer);
+          } else {
+            alert('Failed to save the customer');
+          }
+        });
+
+    } else {
+      alert('Invalid Data, Please Correct...!');
+    }
   }
 
 }
